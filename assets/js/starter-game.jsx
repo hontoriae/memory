458e7a5c3ba64fb1c31d8f65f00150d3a5c810bd
Emily Hontoria <hontoria@ccs.neu.edu>
2018-09-26 19:40:23 -0400

    Reset button and cards

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -3,45 +3,116 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 export default function game_init(root) {
-  ReactDOM.render(<Starter />, root);
+  ReactDOM.render(<MatchGame />, root);
 }
 
-class Starter extends React.Component {
+// State needs:
+// how many matches made
+// total matches available
+// clicks made
+// a list of cards
+
+// Card needs:
+// a letter
+// shown or hidden
+// completed
+
+// Additional functionality: reset button
+
+class MatchGame extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = {
+      matchesComplete: 0,
+      matchesAvailable: 8,
+      clicks: 0,
+      matches: this.newMatches()
+    };
   }
 
+  newMatches() {
+    let letterList = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    let xs = [];
+    _.times(8, () => {
+      let letterIndex = Math.floor(Math.random() * letterList.length);
+      let letterVal = letterList[letterIndex];
+      letterList.splice(letterIndex, 1);
+      let match = { letter: letterVal, show: false, complete: false }
+      xs.push(match);
+      xs.push(match);
+    });
+    return _.shuffle(xs);
+  }
+
+  defaultMatches() {
+    let xs = this.newMatches();
+    let st1 = _.assign(this.state, {
+      matches: xs,
+      matchesComplete: 0,
+      matchesAvailable: 8,
+      clicks: 0
+    });
+    this.setState({ st1 });
+  }
+
+
   swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
+    let state1 = _.assign({}, this.state, {
+      // What goes here? Probably need to copy paste this like 4 times for swapping state things
+    });
     this.setState(state1);
   }
 
-  hax(_ev) {
-    alert("hax!");
+  reset() {
+    // Need to reset the state to the original state,
+    // create new matches, etc.
+  }
+
+  rowRender(start, end) {
+    let toRender = this.state.matches.slice(start, end);
+    console.log(toRender.length);
+    let rendered =
+      _.map(toRender, (card, ii) => {
+        return <div className="column card">
+          {card.letter}
+        </div>
+      });
+    return rendered;
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
+    console.log("render");
+    // let resetButton = <button onClick="reset">Reset</button>
+    let clickTotal = <div>
+      <p>
+        You have clicked: {this.state.clicks} times
+      </p>
+    </div>
+    if (this.state.matchesAvailable == this.state.matchesComplete) {
+      return <div>
+        <p>You win!</p>
       </div>;
     }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
+
+    return (
+      <div id="game">
+        {clickTotal}
+        <button onClick={this.defaultMatches.bind(this)}>Reset</button>
+        <div>
+          <div className="row">
+            {this.rowRender(0, 4)}
+          </div>
+          <div className="row">
+            {this.rowRender(4, 8)}
+          </div>
+          <div className="row">
+            {this.rowRender(8, 12)}
+          </div>
+          <div className="row">
+            {this.rowRender(12, 17)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-
